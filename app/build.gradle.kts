a0fd9a7472442ac0148d2500fb31e4d806d6e1ad
nevadamurdock <nevada.murdock@gmail.com>
2026-09-10 17:55:00 +0700

    fix: remove kotlinOptions (AGP 9.x uses compileOptions)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,10 +56,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         viewBinding = true
     }
