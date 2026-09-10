7e26ac713b2ed47e361fcece65b68c7fc8eae606
nevadamurdock <nevada.murdock@gmail.com>
2026-09-10 19:32:12 +0700

    feat: splash screen + app icon setup for All Might character

---
+++ app/src/main/java/com/allmightgamebooster/gusdev/ui/splash/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.allmightgamebooster.gusdev.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.allmightgamebooster.gusdev.R
+import com.allmightgamebooster.gusdev.data.BoostConfigStore
+import com.allmightgamebooster.gusdev.ui.dashboard.DashboardActivity
+import com.allmightgamebooster.gusdev.ui.onboarding.OnboardingActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = if (BoostConfigStore.isOnboardingDone(this)) {
+                Intent(this, DashboardActivity::class.java)
+            } else {
+                Intent(this, OnboardingActivity::class.java)
+            }
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
